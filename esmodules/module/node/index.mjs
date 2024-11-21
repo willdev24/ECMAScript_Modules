0f626df15f99b05d93c266ed0acebefabdd6389a
willy thiago <128657856+willdev24@users.noreply.github.com>
2024-11-21 14:19:48 -0400

    ECMAScript Modules

---
+++ esmodules/module/node/index.mjs
@@ -0,0 +1,17 @@
+import CurrencyManager from "../shared/currencyManagert.mjs"
+
+
+const update = (value) => {
+  // console.log(value)
+}
+
+const inputMonitor = (value)=> update(CurrencyManager.format(value))
+
+const input = process.openStdin() // modulo interno do node.js quando vc digita um valor de entrada
+console.log(input)
+input.addListener("data", inputMonitor)
+
+
+
+
+
